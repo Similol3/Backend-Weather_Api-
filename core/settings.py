d8070cd52eefadb4c97231c0bd89c82531d545be
Similol3 <matthewotusajo@gmail.com>
2026-01-20 14:44:21 +0000

    chor: Add cors policy to allow all host to connect

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -112,5 +114,9 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 # Configure CORS
-CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:8000"
+]
