4f868e802cda91b226d4ded14163d5a2722a41ca
Similol3 <matthewotusajo@gmail.com>
2026-01-20 14:34:45 +0000

    chor: Add cors policy to allow all host to connect to my api

--- core/settings.py
+++ core/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     
     "weather1",
 ]
@@ -109,3 +110,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+# Configure CORS
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
